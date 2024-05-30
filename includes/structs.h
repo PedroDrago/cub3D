866c9b7779e2cf46b36949fc42b24ea26827d155
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-05-30 20:19:55 -0300

    refactor

--- includes/structs.h
+++ includes/structs.h
@@ -1,6 +1,18 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
+typedef struct s_vector_d
+{
+	double x;
+	double y;
+} t_vector_d;
+
+typedef struct s_vector_i
+{
+	int x;
+	int y;
+} t_vector_i;
+
 typedef struct s_data
 {
 	double posX;
@@ -28,11 +40,31 @@ typedef struct s_image
 	int   height;
 } t_image;
 
-typedef struct s_vector
+typedef struct s_line
 {
-	double x;
-	double y;
-} t_vector;
+	int          height;
+	int          start;
+	int          end;
+	unsigned int color;
+} t_line;
+
+typedef struct s_ray
+{
+	t_vector_i map;
+	t_vector_d camera;
+	t_vector_d dir;
+	// length of ray from current position to next x or y-side
+	t_vector_d side_dist;
+	t_vector_d delta_dist;
+	double     perp_wall_dist;
+	// what direction to step in x or y-direction (either +1 or -1)
+	t_vector_d step;
+	// was there a wall hit?
+	int hit;
+	// was a NS or a EW wall hit?
+	int side;
+
+} t_ray;
 
 typedef struct s_player
 {
@@ -43,20 +75,16 @@ typedef struct s_map
 	char **map;
 } t_map;
 
-typedef struct s_ray
-{
-	t_vector dir;
-} t_ray;
 typedef struct s_game
 {
-	t_data   data;
-	t_player player;
-	t_vector camera;
-	t_ray    ray;
-	double   time;
-	double   old_time;
-	void    *mlx;
-	void    *win;
+	t_data     data;
+	t_player   player;
+	t_vector_d camera;
+	t_ray      ray;
+	double     time;
+	double     old_time;
+	void      *mlx;
+	void      *win;
 } t_game;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -4,15 +4,20 @@
 
 #define mapWidth 24
 #define mapHeight 24
-#define screenWidth 640
-#define screenHeight 480
+#define S_WIDTH 640
+#define S_HEIGHT 480
 #define RGB_RED    0xFF0000
 #define RGB_GREEN  0x00FF00
 #define RGB_BLUE   0x0000FF
 #define RGB_WHITE  0xFFFFFF
 #define RGB_YELLOW 0xFFFF0
 # define RGB_PURPLE 0xA020F0
-# define BROWN 0x964B00
+# define RGB_BROWN 0x964B00
+# define W 119
+# define A 97
+# define S 115
+# define D 100
+# define ESC 65307
 
 int    pressed_keys[5000];
 
@@ -58,162 +63,214 @@ void draw_line(t_image *img, int x, int start, int end, unsigned color)
 		my_mlx_pixel_put(img, x, start++, color);
 }
 
+void get_line(t_line *line, t_ray *ray)
+{
+	line->height = (int)(S_HEIGHT / ray->perp_wall_dist);
+
+	//calculate lowest and highest pixel to fill in current stripe
+	line->start = -line->height / 2 + S_HEIGHT / 2;
+	if(line->start < 0)
+		line->start = 0;
+	line->end = line->height / 2 + S_HEIGHT / 2;
+	if(line->end >= S_HEIGHT)
+		line->end = S_HEIGHT - 1;
+	if (worldMap[ray->map.x][ray->map.y] == 1)
+		line->color = RGB_RED;
+	else if (worldMap[ray->map.x][ray->map.y] == 2)
+		line->color = RGB_GREEN;
+	else if (worldMap[ray->map.x][ray->map.y] == 3)
+		line->color = RGB_BLUE;
+	else if (worldMap[ray->map.x][ray->map.y] == 4)
+		line->color = RGB_WHITE;
+	else
+		line->color = RGB_YELLOW;
+	//give x and y sides different brightness
+	if (ray->side == 1)
+		line->color = line->color / 2;
+}
+
 int key_hook(int key, t_game *game) // <- atualizar essa
 {
-    (void)game;
-    if (key == 65307)
-        exit(1);
-    if (key == 119) //w
-    pressed_keys[119] = !pressed_keys[119];
-    if (key == 97) //e
-        pressed_keys[97] = !pressed_keys[97];
-    if (key == 115) //s
-        pressed_keys[115] = !pressed_keys[115];
-    if (key == 100) //d
-        pressed_keys[100] = !pressed_keys[100];
-    return 0;
+	(void)game;
+	if (key == ESC)
+		exit(1);
+	if (key == W)
+		pressed_keys[W] = !pressed_keys[W];
+	if (key == A)
+		pressed_keys[A] = !pressed_keys[A];
+	if (key == S)
+		pressed_keys[S] = !pressed_keys[S];
+	if (key == D)
+		pressed_keys[D] = !pressed_keys[D];
+	return 0;
+}
+
+void	walk_foward(t_game *game)
+{
+	game->data.posX += game->data.dirX * 0.5;
+	game->data.posY += game->data.dirY * 0.5;
+}
+
+void walk_backwards(t_game *game)
+{
+	game->data.posX -= game->data.dirX * 0.5;
+	game->data.posY -= game->data.dirY * 0.5;
+}
+
+void rotate_left(t_game *game)
+{
+	double oldDirx = game->data.dirX;
+	game->data.dirX = game->data.dirX * cos(0.1) - game->data.dirY * sin(0.1);
+	game->data.dirY = oldDirx * sin(0.1) + game->data.dirY * cos(0.1);
+	double oldPlaneX = game->data.planeX;
+	game->data.planeX = game->data.planeX * cos(0.1) - game->data.planeY * sin(0.1);
+	game->data.planeY = oldPlaneX * sin(0.1) + game->data.planeY * cos(0.1);
+}
+
+void rotate_right(t_game *game)
+{
+	double oldDirx = game->data.dirX;
+	game->data.dirX = game->data.dirX * cos(-0.1) - game->data.dirY * sin(-0.1);
+	game->data.dirY = oldDirx * sin(-0.1) + game->data.dirY * cos(-0.1);
+	double oldPlaneX = game->data.planeX;
+	game->data.planeX = game->data.planeX * cos(-0.1) - game->data.planeY * sin(-0.1);
+	game->data.planeY = oldPlaneX * sin(-0.1) + game->data.planeY * cos(-0.1);
 }
 
 void    update_player_pos(t_game *game) // ESSA AQUI É NOVA
 {
-    if (pressed_keys[119])
-    {
-        game->data.posX += game->data.dirX * 0.5;
-        game->data.posY += game->data.dirY * 0.5;
-    }
-    if (pressed_keys[97])
-    {
-        double oldDirx = game->data.dirX;
-        game->data.dirX = game->data.dirX * cos(0.1) - game->data.dirY * sin(0.1);
-        game->data.dirY = oldDirx * sin(0.1) + game->data.dirY * cos(0.1);
-        double oldPlaneX = game->data.planeX;
-        game->data.planeX = game->data.planeX * cos(0.1) - game->data.planeY * sin(0.1);
-        game->data.planeY = oldPlaneX * sin(0.1) + game->data.planeY * cos(0.1);
-    }
-    if (pressed_keys[115])
-    {
-        game->data.posX -= game->data.dirX * 0.5;
-        game->data.posY -= game->data.dirY * 0.5;
-    }
-    if (pressed_keys[100])
-    {
-        double oldDirx = game->data.dirX;
-        game->data.dirX = game->data.dirX * cos(-0.1) - game->data.dirY * sin(-0.1);
-        game->data.dirY = oldDirx * sin(-0.1) + game->data.dirY * cos(-0.1);
-        double oldPlaneX = game->data.planeX;
-        game->data.planeX = game->data.planeX * cos(-0.1) - game->data.planeY * sin(-0.1);
-        game->data.planeY = oldPlaneX * sin(-0.1) + game->data.planeY * cos(-0.1);
-    }
+	if (pressed_keys[W])
+		walk_foward(game);
+	if (pressed_keys[A])
+		rotate_left(game);
+	if (pressed_keys[S])
+		walk_backwards(game);
+	if (pressed_keys[D])
+		rotate_right(game);
 }
 
+
+void	paint_floor_and_ceiling(t_image *frame)
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (x < S_WIDTH)
+	{
+		y = 0;
+		while (y <= (S_HEIGHT / 2))
+			my_mlx_pixel_put(frame, x, y++, RGB_PURPLE);
+		while (y < S_HEIGHT)
+			my_mlx_pixel_put(frame, x, y++, RGB_BROWN);
+		x++;
+	}
+}
+
+void calculate_step_and_side_dist(t_game *game, t_ray *ray)
+{
+	//calculate step and initial sideDist
+	if (ray->dir.x < 0)
+	{
+		ray->step.x = -1;
+		ray->side_dist.x = (game->data.posX - ray->map.x) * ray->delta_dist.x;
+	}
+	else
+	{
+		ray->step.x = 1;
+		ray->side_dist.x = (ray->map.x + 1.0 - game->data.posX) * ray->delta_dist.x;
+	}
+	if (ray->dir.y < 0)
+	{
+		ray->step.y = -1;
+		ray->side_dist.y = (game->data.posY - ray->map.y) * ray->delta_dist.y;
+	}
+	else
+	{
+		ray->step.y = 1;
+		ray->side_dist.y = (ray->map.y + 1.0 - game->data.posY) * ray->delta_dist.y;
+	}
+
+}
+void	digital_diferencial_analysis(t_ray *ray)
+{
+	while (ray->hit == 0)
+	{
+		//jump to next map square, either in x-direction, or in y-direction
+		if (ray->side_dist.x < ray->side_dist.y)
+		{
+			ray->side_dist.x += ray->delta_dist.x;
+			ray->map.x += ray->step.x;
+			ray->side = 0;
+		}
+		else
+		{
+			ray->side_dist.y += ray->delta_dist.y;
+			ray->map.y += ray->step.y;
+			ray->side = 1;
+		}
+		//Check if ray->has hit a wall
+		if (worldMap[ray->map.x][ray->map.y] > 0) ray->hit = 1;
+	} 
+}
+
+void calculate_distance(t_ray *ray)
+{
+	if(ray->side == 0)
+		ray->perp_wall_dist = (ray->side_dist.x - ray->delta_dist.x);
+	else
+		ray->perp_wall_dist = (ray->side_dist.y - ray->delta_dist.y);
+}
+
+void calculate_pos_dir_delta(t_game *game,t_ray *ray, int x)
+{
+		//calculate ray position and direction
+		ray->camera.x = 2 * x / (double)S_WIDTH - 1; //x-coordinate in camera space
+		ray->dir.x = game->data.dirX + game->data.planeX * ray->camera.x;
+		ray->dir.y = game->data.dirY + game->data.planeY * ray->camera.x;
+		ray->map.x = (int)game->data.posX;
+		ray->map.y = (int)game->data.posY;
+		//length of ray from one x or y-side to next x or y-side
+		if (ray->dir.x == 0)
+			ray->delta_dist.x = 1e30;
+		else
+			ray->delta_dist.x = fabs(1 / ray->dir.x);
+		if (ray->dir.y == 0)
+			ray->delta_dist.y = 1e30;
+		else
+			ray->delta_dist.y = fabs(1 / ray->dir.y);
+}
+
+void	pre_raycast(t_game *game,t_image *frame)
+{
+	static    int    movement_limiter = 0;
+
+	if (++movement_limiter == 6)
+	{
+		movement_limiter = 0;
+		update_player_pos(game);
+	}
+	frame->img = mlx_new_image(game->mlx, S_WIDTH, S_HEIGHT);
+	frame->addr = mlx_get_data_addr(frame->img, &frame->bits_per_pixel, &frame->line_length, &frame->endian);
+	paint_floor_and_ceiling(frame);
+
+}
 int game_loop(t_game *game) // <- Atualizar essa aqui (só ta com a parte de cima da função, o resto pode deixar igual.
 {
-    t_image frame;
-    static    int    movement_limiter = 0;
-    
-    if (++movement_limiter == 6)
-    {
-        movement_limiter = 0;
-        update_player_pos(game);
-    }
-	frame.img = mlx_new_image(game->mlx, screenWidth, screenHeight);
-	frame.addr = mlx_get_data_addr(frame.img, &frame.bits_per_pixel, &frame.line_length, &frame.endian);
-	//Paints ceiling and floor of different colors
-	for (int x = 0; x < screenWidth; x++) {
-		for (int y = 0; y < screenHeight / 2; y++) {
-			my_mlx_pixel_put(&frame, x, y, RGB_PURPLE);
-		}
-		for (int y = screenHeight / 2; y < screenHeight; y++) {
-			my_mlx_pixel_put(&frame, x, y, BROWN);
-		}
-	}
-	for(int x = 0; x < screenWidth; x++)
+	t_image frame;
+	t_ray ray;
+	t_line line;
+
+	pre_raycast(game, &frame);
+	for(int x = 0; x < S_WIDTH; x++)
 	{
-		//calculate ray position and direction
-		double cameraX = 2 * x / (double)screenWidth - 1; //x-coordinate in camera space
-		double rayDirX = game->data.dirX + game->data.planeX * cameraX;
-		double rayDirY = game->data.dirY + game->data.planeY * cameraX;
-		int mapX = (int)game->data.posX;
-		int mapY = (int)game->data.posY;
-
-		//length of ray from current position to next x or y-side
-		double sideDistX;
-		double sideDistY;
-
-		//length of ray from one x or y-side to next x or y-side
-		double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
-		double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
-		double perpWallDist;
-
-		//what direction to step in x or y-direction (either +1 or -1)
-		int stepX;
-		int stepY;
-
-		int hit = 0; //was there a wall hit?
-		int side; //was a NS or a EW wall hit?
-		if (rayDirX < 0)
-		{
-			stepX = -1;
-			sideDistX = (game->data.posX - mapX) * deltaDistX;
-		}
-		else
-		{
-			stepX = 1;
-			sideDistX = (mapX + 1.0 - game->data.posX) * deltaDistX;
-		}
-		if (rayDirY < 0)
-		{
-			stepY = -1;
-			sideDistY = (game->data.posY - mapY) * deltaDistY;
-		}
-		else
-		{
-			stepY = 1;
-			sideDistY = (mapY + 1.0 - game->data.posY) * deltaDistY;
-		}
-		while (hit == 0)
-		{
-			//jump to next map square, either in x-direction, or in y-direction
-			if (sideDistX < sideDistY)
-			{
-				sideDistX += deltaDistX;
-				mapX += stepX;
-				side = 0;
-			}
-			else
-			{
-				sideDistY += deltaDistY;
-				mapY += stepY;
-				side = 1;
-			}
-			//Check if ray has hit a wall
-			if (worldMap[mapX][mapY] > 0) hit = 1;
-		} 
-		if(side == 0) perpWallDist = (sideDistX - deltaDistX);
-		else          perpWallDist = (sideDistY - deltaDistY);
-		//Calculate height of line to draw on screen
-		int lineHeight = (int)(screenHeight / perpWallDist);
-
-		//calculate lowest and highest pixel to fill in current stripe
-		int drawStart = -lineHeight / 2 + screenHeight / 2;
-		if(drawStart < 0)drawStart = 0;
-		int drawEnd = lineHeight / 2 + screenHeight / 2;
-		if(drawEnd >= screenHeight)drawEnd = screenHeight - 1;
-		unsigned int color;
-		switch(worldMap[mapX][mapY])
-		{
-			case 1:  color = RGB_RED;  break; //red
-			case 2:  color = RGB_GREEN;  break; //green
-			case 3:  color = RGB_BLUE;   break; //blue
-			case 4:  color = RGB_WHITE;  break; //white
-			default: color = RGB_YELLOW; break; //yellow
-		}
-
-		//give x and y sides different brightness
-		if (side == 1) {color = color / 2;}
-
-		//draw the pixels of the stripe as a vertical line
-		draw_line(&frame, x, drawStart, drawEnd, color);
+		calculate_pos_dir_delta(game, &ray, x);
+		ray.hit = 0;  
+		calculate_step_and_side_dist(game, &ray);
+		digital_diferencial_analysis(&ray);
+		calculate_distance(&ray);
+		get_line(&line, &ray);
+		draw_line(&frame, x, line.start, line.end, line.color);
 	}
 	mlx_put_image_to_window(game->mlx, game->win, frame.img, 0, 0);
 	mlx_destroy_image(game->mlx, frame.img);
@@ -252,7 +309,7 @@ int main(void)
 	mlx_loop_hook(game.mlx, game_loop, &game);
 	//mlx_key_hook(game.win, key_hook, &game);
 	mlx_hook(game.win, KeyPress, KeyPressMask, &key_hook, 0); // <- ADICIONAR ESSA
-    mlx_hook(game.win, KeyRelease, KeyReleaseMask, &key_hook, 0); //<- ADICIONAR ESSA
+	mlx_hook(game.win, KeyRelease, KeyReleaseMask, &key_hook, 0); //<- ADICIONAR ESSA
 	mlx_loop(game.mlx);
 	return (0);
 }
