8d28877fdf6eb1f85ba7b73ad710596dd01338ba
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-08-29 16:15:00 -0300

    fix minimap overdraw

--- src/movement.c
+++ src/movement.c
@@ -6,8 +6,9 @@ void update_map(char **map, t_vector_d fut_pos, int curr_x, int curr_y)
 {
 	if (curr_x != fut_pos.x || curr_y != fut_pos.y)
 	{
-		map[(int)fut_pos.x][(int)fut_pos.y] = 'P';
 		map[(int)curr_x][(int)curr_y] = '0';
+		map[(int)fut_pos.x][(int)fut_pos.y] = 'P';
+		print_split(map);
 	}
 }
 
