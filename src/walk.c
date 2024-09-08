0ecab80ffe7699c7428975a66df6949fa8176e33
rodriguesrafaelm <rodriguesrafaelm@outlook.com>
2024-09-08 14:42:39 -0300

    refactor: change move forward and backwards algo

--- src/walk.c
+++ src/walk.c
@@ -14,129 +14,126 @@
 
 void	update_map(char **map, t_vector_d fut_pos, int curr_x, int curr_y)
 {
-	if (curr_x != fut_pos.x || curr_y != fut_pos.y)
+	if (curr_x != fut_pos.x || curr_y != fut_pos.y
+		&& is_empty_tile(map[(int)curr_x][(int)curr_y]))
 	{
 		map[(int)curr_x][(int)curr_y] = '0';
 		map[(int)fut_pos.x][(int)fut_pos.y] = 'P';
 	}
 }
 
-void	walk_forward(t_camera *camera, char **map, char **minimap)
+void	walk_forward(t_camera *cam, char **map, char **minimap)
 {
 	t_vector_d	fut_pos;
-	int			diagonal_dir;
 	int			curr_x;
 	int			curr_y;
 
-	fut_pos.x = camera->pos.x + camera->dir.x * camera->mov_speed;
-	fut_pos.y = camera->pos.y + camera->dir.y * camera->mov_speed;
-	diagonal_dir = get_diagonal_dir(camera->pos, fut_pos);
-	if (map[(int)(fut_pos.x)][(int)(fut_pos.y)] == '1')
-		return ;
-	curr_x = (int)(camera->pos.x);
-	curr_y = (int)(camera->pos.y);
-	if (!is_empty_tile(map[(int)(fut_pos.x + 0.2)][curr_y]))
-		fut_pos.x = camera->pos.x;
-	if (camera->dir.x > 0.2 && is_empty_tile(map[(int)(fut_pos.x
-				+ 0.2)][curr_y]))
-		camera->pos.x = fut_pos.x;
-	else if (camera->dir.x < 0.2 && is_empty_tile(map[(int)(fut_pos.x
-				- 0.2)][curr_y]))
-		camera->pos.x = fut_pos.x;
-	if (!is_empty_tile(map[curr_x][(int)(fut_pos.y + 0.2)]))
-		fut_pos.y = camera->pos.y;
-	if (camera->dir.y > 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
-			+ 0.2)]))
-		camera->pos.y = fut_pos.y;
-	else if (camera->dir.y < 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
-			- 0.2)]))
-		camera->pos.y = fut_pos.y;
+	fut_pos.x = cam->pos.x + cam->dir.x * cam->mov_speed;
+	fut_pos.y = cam->pos.y + cam->dir.y * cam->mov_speed;
+	curr_y = (int)(cam->pos.y);
+	curr_x = (int)(cam->pos.x);
+	if (!is_empty_tile(map[(int)(fut_pos.x + 0.2)][curr_y])
+		|| !is_empty_tile(map[(int)(fut_pos.x - 0.2)][curr_y]))
+		fut_pos.x = cam->pos.x;
+	if (cam->dir.x > 0.2 && is_empty_tile(map[(int)(fut_pos.x)][curr_y]))
+		cam->pos.x = fut_pos.x;
+	else if (cam->dir.x < 0.2 && is_empty_tile(map[(int)(fut_pos.x)][curr_y]))
+		cam->pos.x = fut_pos.x;
+	if (!is_empty_tile(map[(int)(cam->pos.x)][(int)(fut_pos.y + 0.2)])
+		|| !is_empty_tile(map[(int)(cam->pos.x)][(int)(fut_pos.y - 0.2)]))
+		fut_pos.y = cam->pos.y;
+	if (cam->dir.y > 0.2
+		&& is_empty_tile(map[(int)(cam->pos.x)][(int)(fut_pos.y)]))
+		cam->pos.y = fut_pos.y;
+	else if (cam->dir.y < 0.2
+		&& is_empty_tile(map[(int)(cam->pos.x)][(int)(fut_pos.y)]))
+		cam->pos.y = fut_pos.y;
 	update_map(minimap, fut_pos, curr_x, curr_y);
 }
 
-void	walk_backwards(t_camera *camera, char **map, char **minimap)
+void	walk_backwards(t_camera *cam, char **map, char **minimap)
 {
+	t_vector_d	fut_pos;
 	int			curr_x;
 	int			curr_y;
-	t_vector_d	fut_pos;
 
-	fut_pos.x = camera->pos.x - camera->dir.x * camera->mov_speed;
-	fut_pos.y = camera->pos.y - camera->dir.y * camera->mov_speed;
-	curr_x = (int)(camera->pos.x);
-	curr_y = (int)(camera->pos.y);
-	if (map[(int)(fut_pos.x)][(int)(fut_pos.y)] == '1')
-		return ;
-	if (camera->dir.x > 0.2 && is_empty_tile(map[(int)(fut_pos.x
-				- 0.2)][curr_y]) && is_empty_tile(map[(int)(fut_pos.x
-				+ 0.2)][curr_y]))
-		camera->pos.x = fut_pos.x;
-	else if (camera->dir.x < 0.2 && is_empty_tile(map[(int)(fut_pos.x
-				+ 0.2)][curr_y]) && is_empty_tile(map[(int)(fut_pos.x
-				- 0.2)][curr_y]))
-		camera->pos.x = fut_pos.x;
-	if (camera->dir.y > 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
-			- 0.2)]) && is_empty_tile(map[curr_x][(int)(fut_pos.y + 0.2)]))
-		camera->pos.y = fut_pos.y;
-	else if (camera->dir.y < 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
-			+ 0.2)]) && is_empty_tile(map[curr_x][(int)(fut_pos.y - 0.2)]))
-		camera->pos.y = fut_pos.y;
+	fut_pos.x = cam->pos.x - cam->dir.x * cam->mov_speed;
+	fut_pos.y = cam->pos.y - cam->dir.y * cam->mov_speed;
+	curr_y = (int)(cam->pos.y);
+	curr_x = (int)(cam->pos.x);
+	if (!is_empty_tile(map[(int)(fut_pos.x + 0.2)][curr_y])
+		|| !is_empty_tile(map[(int)(fut_pos.x - 0.2)][curr_y]))
+		fut_pos.x = cam->pos.x;
+	if (cam->dir.x > 0.2 && is_empty_tile(map[(int)(fut_pos.x)][curr_y]))
+		cam->pos.x = fut_pos.x;
+	else if (cam->dir.x < 0.2 && is_empty_tile(map[(int)(fut_pos.x)][curr_y]))
+		cam->pos.x = fut_pos.x;
+	if (!is_empty_tile(map[(int)(cam->pos.x)][(int)(fut_pos.y + 0.2)])
+		|| !is_empty_tile(map[(int)(cam->pos.x)][(int)(fut_pos.y - 0.2)]))
+		fut_pos.y = cam->pos.y;
+	if (cam->dir.y > 0.2
+		&& is_empty_tile(map[(int)(cam->pos.x)][(int)(fut_pos.y)]))
+		cam->pos.y = fut_pos.y;
+	else if (cam->dir.y < 0.2
+		&& is_empty_tile(map[(int)(cam->pos.x)][(int)(fut_pos.y)]))
+		cam->pos.y = fut_pos.y;
 	update_map(minimap, fut_pos, curr_x, curr_y);
 }
 
-void	walk_left(t_camera *camera, char **map, char **minimap)
+void	walk_left(t_camera *cam, char **map, char **minimap)
 {
 	int			curr_x;
 	int			curr_y;
 	t_vector_d	fut_pos;
 
-	fut_pos.x = camera->pos.x - camera->plane.x * camera->mov_speed;
-	fut_pos.y = camera->pos.y - camera->plane.y * camera->mov_speed;
-	curr_x = (int)(camera->pos.x);
-	curr_y = (int)(camera->pos.y);
+	fut_pos.x = cam->pos.x - cam->plane.x * cam->mov_speed;
+	fut_pos.y = cam->pos.y - cam->plane.y * cam->mov_speed;
+	curr_x = (int)(cam->pos.x);
+	curr_y = (int)(cam->pos.y);
 	if (map[(int)(fut_pos.x)][(int)(fut_pos.y)] == '1')
 		return ;
-	if (camera->dir.x > 0.2 && is_empty_tile(map[(int)(fut_pos.x
+	if (cam->dir.x > 0.2 && is_empty_tile(map[(int)(fut_pos.x
 				- 0.2)][curr_y]) && is_empty_tile(map[(int)(fut_pos.x
 				+ 0.2)][curr_y]))
-		camera->pos.x = fut_pos.x;
-	else if (camera->dir.x < 0.2 && is_empty_tile(map[(int)(fut_pos.x
+		cam->pos.x = fut_pos.x;
+	else if (cam->dir.x < 0.2 && is_empty_tile(map[(int)(fut_pos.x
 				+ 0.2)][curr_y]) && is_empty_tile(map[(int)(fut_pos.x
 				- 0.2)][curr_y]))
-		camera->pos.x = fut_pos.x;
-	if (camera->dir.y > 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
+		cam->pos.x = fut_pos.x;
+	if (cam->dir.y > 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
 			+ 0.2)]) && is_empty_tile(map[curr_x][(int)(fut_pos.y - 0.2)]))
-		camera->pos.y = fut_pos.y;
-	else if (camera->dir.y < 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
+		cam->pos.y = fut_pos.y;
+	else if (cam->dir.y < 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
 			- 0.2)]) && is_empty_tile(map[curr_x][(int)(fut_pos.y + 0.2)]))
-		camera->pos.y = fut_pos.y;
+		cam->pos.y = fut_pos.y;
 	update_map(minimap, fut_pos, curr_x, curr_y);
 }
 
-void	walk_right(t_camera *camera, char **map, char **minimap)
+void	walk_right(t_camera *cam, char **map, char **minimap)
 {
 	int			curr_x;
 	int			curr_y;
 	t_vector_d	fut_pos;
 
-	fut_pos.x = camera->pos.x + camera->plane.x * camera->mov_speed;
-	fut_pos.y = camera->pos.y + camera->plane.y * camera->mov_speed;
-	curr_x = (int)(camera->pos.x);
-	curr_y = (int)(camera->pos.y);
+	fut_pos.x = cam->pos.x + cam->plane.x * cam->mov_speed;
+	fut_pos.y = cam->pos.y + cam->plane.y * cam->mov_speed;
+	curr_x = (int)(cam->pos.x);
+	curr_y = (int)(cam->pos.y);
 	if (map[(int)(fut_pos.x)][(int)(fut_pos.y)] == '1')
 		return ;
-	if (camera->dir.x > 0.2 && is_empty_tile(map[(int)(fut_pos.x
+	if (cam->dir.x > 0.2 && is_empty_tile(map[(int)(fut_pos.x
 				+ 0.2)][curr_y]) && is_empty_tile(map[(int)(fut_pos.x
 				- 0.2)][curr_y]))
-		camera->pos.x = fut_pos.x;
-	else if (camera->dir.x < 0.2 && is_empty_tile(map[(int)(fut_pos.x
+		cam->pos.x = fut_pos.x;
+	else if (cam->dir.x < 0.2 && is_empty_tile(map[(int)(fut_pos.x
 				- 0.2)][curr_y]) && is_empty_tile(map[(int)(fut_pos.x
 				+ 0.2)][curr_y]))
-		camera->pos.x = fut_pos.x;
-	if (camera->dir.y > 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
+		cam->pos.x = fut_pos.x;
+	if (cam->dir.y > 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
 			+ 0.2)]) && is_empty_tile(map[curr_x][(int)(fut_pos.y - 0.2)]))
-		camera->pos.y = fut_pos.y;
-	else if (camera->dir.y < 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
+		cam->pos.y = fut_pos.y;
+	else if (cam->dir.y < 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
 			- 0.2)]) && is_empty_tile(map[curr_x][(int)(fut_pos.y + 0.2)]))
-		camera->pos.y = fut_pos.y;
+		cam->pos.y = fut_pos.y;
 	update_map(minimap, fut_pos, curr_x, curr_y);
 }
