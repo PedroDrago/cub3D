f3aa853bd1a4e73dc1c0f9ae62bbdb9150d34aaa
rodriguesrafaelm <rodriguesrafaelm@outlook.com>
2024-09-08 15:29:34 -0300

    wip: adding mouse movement

--- src/main.c
+++ src/main.c
@@ -12,6 +12,23 @@
 
 #include "../includes/cub3d.h"
 
+int mouse_hook(int x, int y, t_game *game)
+{
+	int	delta;
+
+	delta = x - S_WIDTH / 2;
+	game->camera.dir.x = game->camera.dir.x * cos(-delta * 0.001)
+		- game->camera.dir.y * sin(-delta * 0.001);
+	game->camera.dir.y = game->camera.dir.x * sin(-delta * 0.001)
+		+ game->camera.dir.y * cos(-delta * 0.001);
+	game->camera.plane.x = game->camera.plane.x * cos(-delta * 0.001)
+		- game->camera.plane.y * sin(-delta * 0.001);
+	game->camera.plane.y = game->camera.plane.x * sin(-delta * 0.001)
+		+ game->camera.plane.y * cos(-delta * 0.001);
+	mlx_mouse_move(game->mlx, game->win, S_WIDTH / 2, S_HEIGHT / 2);
+	return (0);
+}
+
 int	main(int argc, char *argv[])
 {
 	t_game	game;
@@ -25,7 +42,9 @@ int	main(int argc, char *argv[])
 	game.win = mlx_new_window(game.mlx, S_WIDTH, S_HEIGHT, "Cub3D");
 	init_camera(&game.camera, &game);
 	init_textures(&game);
+	mlx_mouse_move(game.mlx, game.win, S_WIDTH / 2, S_HEIGHT / 2);
 	mlx_loop_hook(game.mlx, game_loop, &game);
+	mlx_hook(game.win, MotionNotify, PointerMotionMask, &mouse_hook, &game);
 	mlx_hook(game.win, KeyPress, KeyPressMask, &key_hook_down, &game);
 	mlx_hook(game.win, KeyRelease, KeyReleaseMask, &key_hook_up, &game);
 	mlx_hook(game.win, DestroyNotify, StructureNotifyMask, clean_exit, &game);
