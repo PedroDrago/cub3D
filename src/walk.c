69118f8b12a7798fd9c230459381debe3e2bca3a
rodriguesrafaelm <rodriguesrafaelm@outlook.com>
2024-09-08 14:57:29 -0300

    refactor: change move left and right algo

--- src/walk.c
+++ src/walk.c
@@ -59,8 +59,8 @@ void	walk_backwards(t_camera *cam, char **map, char **minimap)
 
 	fut_pos.x = cam->pos.x - cam->dir.x * cam->mov_speed;
 	fut_pos.y = cam->pos.y - cam->dir.y * cam->mov_speed;
-	curr_y = (int)(cam->pos.y);
 	curr_x = (int)(cam->pos.x);
+	curr_y = (int)(cam->pos.y);
 	if (!is_empty_tile(map[(int)(fut_pos.x + 0.2)][curr_y])
 		|| !is_empty_tile(map[(int)(fut_pos.x - 0.2)][curr_y]))
 		fut_pos.x = cam->pos.x;
@@ -90,21 +90,21 @@ void	walk_left(t_camera *cam, char **map, char **minimap)
 	fut_pos.y = cam->pos.y - cam->plane.y * cam->mov_speed;
 	curr_x = (int)(cam->pos.x);
 	curr_y = (int)(cam->pos.y);
-	if (map[(int)(fut_pos.x)][(int)(fut_pos.y)] == '1')
-		return ;
-	if (cam->dir.x > 0.2 && is_empty_tile(map[(int)(fut_pos.x
-				- 0.2)][curr_y]) && is_empty_tile(map[(int)(fut_pos.x
-				+ 0.2)][curr_y]))
+	if (!is_empty_tile(map[(int)(fut_pos.x + 0.2)][curr_y])
+		|| !is_empty_tile(map[(int)(fut_pos.x - 0.2)][curr_y]))
+		fut_pos.x = cam->pos.x;
+	if (cam->dir.x < 0.2 && is_empty_tile(map[(int)(fut_pos.x)][curr_y]))
 		cam->pos.x = fut_pos.x;
-	else if (cam->dir.x < 0.2 && is_empty_tile(map[(int)(fut_pos.x
-				+ 0.2)][curr_y]) && is_empty_tile(map[(int)(fut_pos.x
-				- 0.2)][curr_y]))
+	else if (cam->dir.x > 0.2 && is_empty_tile(map[(int)(fut_pos.x)][curr_y]))
 		cam->pos.x = fut_pos.x;
-	if (cam->dir.y > 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
-			+ 0.2)]) && is_empty_tile(map[curr_x][(int)(fut_pos.y - 0.2)]))
+	if (!is_empty_tile(map[(int)(cam->pos.x)][(int)(fut_pos.y + 0.2)])
+		|| !is_empty_tile(map[(int)(cam->pos.x)][(int)(fut_pos.y - 0.2)]))
+		fut_pos.y = cam->pos.y;
+	if (cam->dir.y > 0.2
+		&& is_empty_tile(map[curr_x][(int)(fut_pos.y)]))
 		cam->pos.y = fut_pos.y;
-	else if (cam->dir.y < 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
-			- 0.2)]) && is_empty_tile(map[curr_x][(int)(fut_pos.y + 0.2)]))
+	else if (cam->dir.y < 0.2
+		&& is_empty_tile(map[curr_x][(int)(fut_pos.y)]))
 		cam->pos.y = fut_pos.y;
 	update_map(minimap, fut_pos, curr_x, curr_y);
 }
@@ -119,21 +119,21 @@ void	walk_right(t_camera *cam, char **map, char **minimap)
 	fut_pos.y = cam->pos.y + cam->plane.y * cam->mov_speed;
 	curr_x = (int)(cam->pos.x);
 	curr_y = (int)(cam->pos.y);
-	if (map[(int)(fut_pos.x)][(int)(fut_pos.y)] == '1')
-		return ;
-	if (cam->dir.x > 0.2 && is_empty_tile(map[(int)(fut_pos.x
-				+ 0.2)][curr_y]) && is_empty_tile(map[(int)(fut_pos.x
-				- 0.2)][curr_y]))
+	if (!is_empty_tile(map[(int)(fut_pos.x + 0.2)][curr_y])
+		|| !is_empty_tile(map[(int)(fut_pos.x - 0.2)][curr_y]))
+		fut_pos.x = cam->pos.x;
+	if (cam->dir.x > 0.2 && is_empty_tile(map[(int)(fut_pos.x)][curr_y]))
 		cam->pos.x = fut_pos.x;
-	else if (cam->dir.x < 0.2 && is_empty_tile(map[(int)(fut_pos.x
-				- 0.2)][curr_y]) && is_empty_tile(map[(int)(fut_pos.x
-				+ 0.2)][curr_y]))
+	else if (cam->dir.x < 0.2 && is_empty_tile(map[(int)(fut_pos.x)][curr_y]))
 		cam->pos.x = fut_pos.x;
-	if (cam->dir.y > 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
-			+ 0.2)]) && is_empty_tile(map[curr_x][(int)(fut_pos.y - 0.2)]))
+	if (!is_empty_tile(map[(int)(cam->pos.x)][(int)(fut_pos.y + 0.2)])
+		|| !is_empty_tile(map[(int)(cam->pos.x)][(int)(fut_pos.y - 0.2)]))
+		fut_pos.y = cam->pos.y;
+	if (cam->dir.y > 0.2
+		&& is_empty_tile(map[curr_x][(int)(fut_pos.y)]))
 		cam->pos.y = fut_pos.y;
-	else if (cam->dir.y < 0.2 && is_empty_tile(map[curr_x][(int)(fut_pos.y
-			- 0.2)]) && is_empty_tile(map[curr_x][(int)(fut_pos.y + 0.2)]))
+	else if (cam->dir.y < 0.2
+		&& is_empty_tile(map[curr_x][(int)(fut_pos.y)]))
 		cam->pos.y = fut_pos.y;
 	update_map(minimap, fut_pos, curr_x, curr_y);
 }
